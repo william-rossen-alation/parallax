528748c742f07da11df28853b708d5e6eda71300
William Rossen <william.rossen@alation.com>
2025-08-25 11:52:41 -0400

    working parallax

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,41 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Lenis from "@studio-freight/lenis";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 const images = [
-  "1.jpg",
-  "2.jpg",
-  "3.jpg",
-  "4.jpg",
-  "5.jpg",
-  "6.jpg",
-  "7.jpg",
-  "8.jpg",
-  "9.jpg",
-  "10.jpg",
-  "11.jpg",
-  "12.jpg",
+  "1.avif",
+  "2.avif",
+  "3.avif",
+  "4.avif",
+  "5.avif",
+  "6.avif",
+  "7.avif",
+  "8.avif",
+  "9.avif",
+  "10.avif",
+  "11.avif",
+  "12.avif",
 ]
 
 export default function Home() {
+
+  const gallery = useRef(null);
+  const [dimension, setDimension] = useState({width: 0, height: 0});
+
+  const { scrollYProgress } = useScroll({
+    target: gallery,
+    offset: ['start end', 'end start']
+  });
+
+  const { height } = dimension;
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+
   useEffect(() => {
     const lenis = new Lenis();
 
@@ -28,27 +44,39 @@ export default function Home() {
       requestAnimationFrame(raf);
     }
 
+    const resize = () => {
+      setDimension({width: window.innerWidth, height: window.innerHeight});
+    }
+
+    window.addEventListener('resize', resize);
     requestAnimationFrame(raf);
+    resize();
+
+    return () => {
+      window.removeEventListener('resize', resize);
+    }
   }, []);
 
   return (
     <main className={styles.main}>
       <div className={styles.spacer}></div>
-      <div className={styles.gallery}>
-        <div className={styles.galleryWrapper}>
-          <Column images={[images[0], images[1], images[2]]} />
-          <Column images={[images[3], images[4], images[5]]} />
-          <Column images={[images[6], images[7], images[8]]} />
-          <Column images={[images[9], images[10], images[11]]} />
-        </div>
+      <div ref={gallery} className={styles.gallery}>
+        <Column images={[images[0], images[1], images[2]]} y={y} />
+        <Column images={[images[3], images[4], images[5]]} y={y2} />
+        <Column images={[images[6], images[7], images[8]]} y={y3} />
+        <Column images={[images[9], images[10], images[11]]} y={y4} />
       </div>
+      <div className={styles.spacer}></div>
     </main>
   );
 }
 
-const Column = ({images}: {images: string[]}) => {
+const Column = ({images, y}: {images: string[], y: any}) => {
   return (
-    <div className={styles.column}>
+    <motion.div
+     className={styles.column}
+     style={{y}}
+    >
       {images.map((src: string, i: number) => {
         return (
           <div key={i} className={styles.imageContainer}>
@@ -60,6 +88,6 @@ const Column = ({images}: {images: string[]}) => {
           </div>
         )
       })}
-    </div>
+    </motion.div>
   )
 }
